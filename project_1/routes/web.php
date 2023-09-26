7fa92287f66aba31e4443946f9d18efab23a844a
AMSusz <casimir3thegreat@gmail.com>
2023-09-26 21:11:23 +0200

    l2 routes

--- project_1/routes/web.php
+++ project_1/routes/web.php
@@ -28,4 +28,47 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// ------
+
+Route::get('/city_old', function(){
+    return "Miasto";
+});
+
+Route::get('/city', function(){
+    return view('city');
+});
+
+Route::redirect(uri:'/', destination:'/city_old');
+
+Route::redirect(uri:'/', destination:'/city', status:301);
+
+Route::get('/status', function(){
+    return response()->json([
+        'error' => false,
+        'code' => 203,
+        'message' => 'everything works just fine!'
+    ], 500)->status();
+    
+});
+
+Route::get('city2', function(){
+    // return ["name" => "Janusz", "city" => "Poznań"];
+    return view('city', ["name" => "Janusz", "city" => "Poznań"]);
+});
+
+Route::get('page/{x}', function($x){
+    return $x;
+});
+
+Route::get('pages/{x}', function($x){
+    $info = [
+        "about" => "Informacje o stronie",
+        "contact" => "contact@o2.pl",
+        "home" => "Strona domowa"
+    ];
+    return $info[$x];
+});
+
+// ------
+
 require __DIR__.'/auth.php';
